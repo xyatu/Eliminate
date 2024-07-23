5ef4c448597c2048d4d332ba0091db0c40462fe2
Echoes4 <echoes_11@163.com>
2024-07-23 15:03:39 +0800

    修复了滑动和购买冲突的问题

--- assets/module_build/ui/ui_building/UI_Building.ts
+++ assets/module_build/ui/ui_building/UI_Building.ts
@@ -1,4 +1,4 @@
-import { _decorator, Color, Component, EventTouch, instantiate, log, Node, Prefab, Sprite, UITransform, v3, Vec3, view } from 'cc';
+import { _decorator, Color, Component, EventTouch, instantiate, log, Node, Prefab, Sprite, UITransform, v3, Vec2, Vec3, view } from 'cc';
 import { Building, DataGetter, Sound } from '../../../start/DataGetter';
 import { BuildingState } from '../../script/building/BuildingState';
 import { BuilderComp } from '../../script/manager/BuilderComp';
@@ -21,12 +21,19 @@ export class UI_Building extends Component {
     building_data: Building = null;
 
     isLock: boolean = true;
+    originPos: Vec2 = new Vec2();
 
     start() {
+        this.node.on(Node.EventType.TOUCH_START, this.onTouchStart, this);
         this.node.on(Node.EventType.TOUCH_END, this.onTouchEnd, this);
     }
 
+    onTouchStart(event: EventTouch) {
+        this.originPos = event.getLocation();
+    }
+
     onTouchEnd(event: EventTouch) {
+        if (this.originPos.subtract(event.getLocation()).length() > 50) return;
         let sound: Sound = DataGetter.inst.sound.get(SoundConfig.build_select);
         tgxAudioMgr.inst.playOneShot(sound.audio, sound.volumn);
         if (this.isLock) {
@@ -47,7 +54,7 @@ export class UI_Building extends Component {
 
         if (this.isLock) {
             this.node.getChildByName('Sprite').getComponent(Sprite).color = new Color(180, 180, 180, 255);
-            this.node.getChildByName('Lock').active = true  ;
+            this.node.getChildByName('Lock').active = true;
         }
     }
 
